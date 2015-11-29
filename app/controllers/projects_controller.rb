7332d579482ca6805a3e7da04a7233ef959b5f9a
Giovani <Giovani@Giovanis-MacBook-Pro.local>
2015-11-28 22:16:02 -0800

    Setup and styled basic project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,40 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@projects = Project.all.order("created_at desc")
+	end
+
+	def new
+		@projects = Project.new
+	end
+
+	def create
+
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "Nice Stephanie! That poject was succesfully saved!"
+	else
+		render 'new'
+	end
+	end
+
+	def show
+		
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+		
+	end
+
+
+
+end
